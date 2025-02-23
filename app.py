c560ff6889eebdd2c3345f511d56c4cfd87e76be
ahmedis123 <68246919+ahmedis123@users.noreply.github.com>
2025-02-23 12:38:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class Phone(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     brand = db.Column(db.String(50), nullable=False)
-    price = db.Column(db.Float), nullable=False)
+    price = db.Column(db.Float, nullable=False)
     condition = db.Column(db.String(20), nullable=False)  # جديد أو مستعمل
     image = db.Column(db.String(200), nullable=False)
 
